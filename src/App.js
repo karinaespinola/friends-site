114d2359200c37bb189aa540013b9c4328993ef2
Karina Espinola <k.espinolah@gmail.com>
2022-01-05 23:13:02 -0600

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './styles/index.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
+      <Hero/>
       <h1>Holi</h1>
     </div>
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='w-screen h-96' style={{ background: "url('/img/cast-walking.jpeg')"}}>
+      
+    </div>
+  )
+}
+
+export default Hero
