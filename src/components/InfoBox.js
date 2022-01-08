74a6f172d0049ad45508f3970e7a47ea1bbb9a60
Karina Espinola <k.espinolah@gmail.com>
2022-01-08 16:19:19 -0600

    Changes to InfoBox

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InfoBox = ({ bgColor, bgOpacity, titleColor, textColor, title, text }) => {
-  const backgroundColor = `bg-${bgColor}${typeof bgOpacity !== 'undefined' ? `/${bgOpacity}` : ''}`;
-
+const InfoBox = ({ title, text }) => {
+  
   return (
-    <section className={` ${backgroundColor} p-4`}>
-      <h1>{ title }</h1>
-      <p>{ text }</p>
+    <section className="bg-gray-800/80">
+      <h1 className='text-white text-4xl'>{ title }</h1>
+      <p className='text-white'>{ text }</p>
     </section>
   )
 }
 
 InfoBox.propTypes = {
-  bgColor: PropTypes.string.isRequired,
-  bgOpacity: PropTypes.string,
-  titleColor: PropTypes.string,
-  textColor: PropTypes.string,
   title: PropTypes.string,
   text: PropTypes.string
-}
-
-InfoBox.defaultProps = {
-  titleColor: 'white',
-  textColor: 'white'
 }
 
 export default InfoBox
