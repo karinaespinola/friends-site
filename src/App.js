68bd5a0444ad1efefbc83806054dcdd790d56211
Karina Espinola <k.espinolah@gmail.com>
2022-01-11 22:51:10 -0600

    Add some styles to the quote

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './index.css';
 import Gallery from './components/Gallery';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <div className="w-screen bg-center bg-cover bg-[url('../public/img/cast-walking.jpeg')] p-32 md:p-80">
         <section className="container mx-auto grid grid-cols-12 relative">
+        <div className="col-span-12">
+          <Quote />
+        </div>
           <div className="col-span-12 absolute mt-10 md:mt-72">
             <Gallery />
           </div>
---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Quote = ({ quote }) => {
+  return (
+    <div className='text-white shadow-sm text-5xl'>
+      Lorem Ipsum Lorem Ipsum Lorem Ipsum
+    </div>
+  )
+}
+
+export default Quote
