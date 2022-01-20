67277bb9c75a8ae513dee4439b817a7c66d97efd
Karina Espinola <k.espinolah@gmail.com>
2022-01-20 17:07:26 -0600

    Fix quote and avatars positions

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Quote from './components/Quote';
 function App() {
   return (
     <div className="App">
-      <div className="w-screen bg-center bg-cover bg-[url('../public/img/cast-walking.jpeg')] p-32 md:p-80">
-        <section className="container mx-auto grid grid-cols-12 relative">
-        <div className="col-span-12">
-          <Quote />
-        </div>
-          <div className="col-span-12 absolute mt-10 md:mt-72">
+      <div className="w-screen bg-center bg-cover bg-[url('../public/img/cast-walking.jpeg')]">
+        <section className="container mx-auto grid grid-cols-12 h-96 relative">
+          <div className="col-span-12 absolute top-1/4">
+            <Quote />
+          </div>
+          <div className="col-span-10 absolute top-3/4">
             <Gallery />
           </div>
         </section>
